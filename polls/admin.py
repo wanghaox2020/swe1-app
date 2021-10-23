2e4f9e43e1573df682196f938604e88ba1ec9f50
kevinking <hw2766@nyu.edu>
2021-10-23 14:24:34 -0400

    test

--- polls/admin.py
+++ polls/admin.py
@@ -2,28 +2,29 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-#admin.site.register(Question)
-#admin.site.register(Choice)
+# admin.site.register(Question)
+# admin.site.register(Choice)
 
-#admin.site.site_header = "Polls admin"
-#admin.site.site_title = "Polls admin area"
-#admin.site.index_title = "Wecome to polls admin area"
+# admin.site.site_header = "Polls admin"
+# admin.site.site_title = "Polls admin area"
+# admin.site.index_title = "Wecome to polls admin area"
 
-class ChoiceInline (admin.TabularInline):
+
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 
-
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
